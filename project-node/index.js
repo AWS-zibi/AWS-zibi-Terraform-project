73b9f8b957ecb8601bf90646a2f022e028f22b4a
AWS-zibi <awszibi@gmail.com>
2025-09-03 08:30:33 +0000

    added new project

---
+++ project-node/index.js
@@ -0,0 +1,29 @@
+[1:58 PM] Prakash- Devops
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+ 
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/") {
+
+    res.writeHead(200, {"Content-Type": "text/plain"});
+
+    res.end("Hello World from ECS Fargate!\n");
+
+  } else {
+
+    res.writeHead(404);
+
+    res.end("Not found\n");
+
+  }
+
+});
+ 
+server.listen(port, () => {
+
+  console.log(`Server listening on port ${port}`);
+
+});
+ 
