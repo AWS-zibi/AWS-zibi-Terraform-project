f0a94d0d1c2d8c17257f8c77705a9aaaab30a914
AWS-zibi <awszibi@gmail.com>
2025-09-03 08:24:35 +0000

    delete project

--- node.js/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
-      <h1>🚀Hello World from Next.js on ECS And Using Terraform As Well 🚀</h1>
-    </main>
-  );
-}
